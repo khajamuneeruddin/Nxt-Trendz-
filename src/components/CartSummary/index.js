57f5476242fbda77173f9e864149afe6f65af1ec
khajamuneeruddin <khwajaurs@gmail.com>
2024-04-27 10:12:39 +0000

    second commit

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -18,21 +18,21 @@ const CartSummary = () => (
         0,
       )
       return (
-        <div className="cart-summary-container">
-          <div className="cart-summary-card">
-            <h1 className="cart-items-total-price">
+        <div className='cart-summary-container'>
+          <div className='cart-summary-card'>
+            <h1 className='cart-items-total-price'>
               Order Total: <span>RS {totalPrice}/-</span>
             </h1>
-            <p className="cart-items-count">{items} Items in cart</p>
+            <p className='cart-items-count'>{items} Items in cart</p>
 
             <Popup
               modal
               trigger={
-                <button className="checkout-btn" type="button">
+                <button className='checkout-btn' type='button'>
                   Checkout
                 </button>
               }
-              position="top left"
+              position='top left'
             >
               {close => <Payment close={close} />}
             </Popup>
